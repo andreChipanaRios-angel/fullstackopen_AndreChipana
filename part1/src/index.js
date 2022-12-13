f17a1be99f003f29b07aecb5d8119bd6d73643a0
andreChipanaRios-angel <119696005+andreChipanaRios-angel@users.noreply.github.com>
2022-12-12 18:50:57 -0600

    1.4: course information step 4

--- part1/src/index.js
+++ part1/src/index.js
@@ -31,18 +31,20 @@ const Total = (props) =>{
 }*/
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     /*<>
       <Header course = {course}/> 
@@ -57,7 +59,7 @@ const App = () => {
       />
       <Total sumExercises={sumExercises}/>
     </>*/
-    <>
+    /*<>
       <h1>{course}</h1>
       <p>
         {part1.name} {part1.exercises}
@@ -68,6 +70,13 @@ const App = () => {
       <p>
         {part3.name} {part3.exercises}
       </p>
+    </>*/
+    <>
+      <h1>{course}</h1>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
+    
     </>
 
   )
